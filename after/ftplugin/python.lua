0ec02366727712ebaf8cadc53794207f32f42efb
Allestor Oakley <allestoroakley@outlook.com>
2025-08-19 01:28:23 +0700

    feat: ipython repl <nl> keymap improvement
    will now open a new ipython repl if there is no opened ipython repl
    terminal

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,27 @@
 -- Python repl keymaps
 vim.keymap.set("n", "<NL>", function()
+    -- check if ipython exist, if not, open a new ipython repl
+    local toggleterm_ok, toggleterm_terminal = pcall(require, "toggleterm.terminal")
+    if toggleterm_ok then
+        local terms = toggleterm_terminal.get_all()
+        local iprepl_exist = false
+        for _, v in pairs(terms) do
+            if v["cmd"] then
+                if string.find(v["cmd"], "ipython") then
+                    iprepl_exist = true
+                    break
+                end
+            end
+        end
+        if not iprepl_exist then
+            vim.cmd [[IPRepl]]
+            vim.api.nvim_feedkeys(
+                vim.api.nvim_replace_termcodes("<C-\\><C-n><C-w><c-p>", true, true, true),
+                "n",
+                true
+            )
+        end
+    end
     vim.api.nvim_feedkeys(
         vim.api.nvim_replace_termcodes("v/^# %%<cr>kogNj", true, true, true),
         "n",
