030e26121335008d2ec3d61124e94dbc1fb11cab
Allestor Oakley <allestoroakley@outlook.com>
2025-09-04 00:32:34 +0700

    feat: improve markdown

--- lua/plugins/util/markdown.lua
+++ lua/plugins/util/markdown.lua
@@ -26,6 +26,16 @@ return {
         'MeanderingProgrammer/render-markdown.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
         ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
-        opts = {},
+        opts = {
+            preset = 'obsidian',
+            completions = { blink = { enabled = true } },
+            code = {
+                language_border = '',
+            },
+        },
+        config = function(_, opts)
+            vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = nil })
+            require('render-markdown').setup(opts)
+        end
     },
 }
