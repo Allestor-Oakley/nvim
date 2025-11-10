8c22375f3a506d8bcb75fd2a30e31776004a87f8
Allestor Oakley <allestoroakley@outlook.com>
2025-11-10 13:44:03 +0700

    feat: lazy load refactoring

--- lua/plugins/util/refactoring.lua
+++ lua/plugins/util/refactoring.lua
@@ -1,50 +1,55 @@
+local function keymap(keys, action, desc)
+    return {
+        keys,
+        mode = { "n", "x" },
+        function() return action end,
+        noremap = true,
+        silent = true,
+        desc = "Refactor: " .. desc,
+        expr = true
+    }
+end
+
 return {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local keymap = function(keys, action, desc)
-            vim.keymap.set({ "n", "x" }, keys, action,
-                { noremap = true, silent = true, desc = "Refactor: " .. desc, expr = true })
-        end
-
-        vim.keymap.set(
-            { "n", "x" },
-            "<leader>rr",
-            function() require('refactoring').select_refactor() end,
-            {
-                desc = "Refactor: Select [R]efactor",
-                silent = true,
-            }
-        )
+    keys = {
         keymap("<leader>re", function()
             return require('refactoring').refactor('Extract Function')
-        end, "[E]xtract function")
+        end, "[E]xtract function"),
         keymap("<leader>rf", function()
             return require('refactoring').refactor('Extract Function To File')
-        end, "Extract function to [f]ile")
+        end, "Extract function to [f]ile"),
 
         keymap("<leader>rv", function()
             return require('refactoring').refactor('Extract Variable')
-        end, "Extract [V]ariable")
+        end, "Extract [V]ariable"),
 
         keymap("<leader>ri", function()
             return require('refactoring').refactor('Inline Variable')
-        end, "Inline Variable")
+        end, "Inline Variable"),
         keymap("<leader>rI", function()
             return require('refactoring').refactor('Inline Function')
-        end, "Inline Function")
+        end, "Inline Function"),
 
         keymap("<leader>rb", function()
             return require('refactoring').refactor('Extract Block')
-        end, "Extract [B]lock")
+        end, "Extract [B]lock"),
         keymap("<leader>rt", function()
             return require('refactoring').refactor('Extract Block To File')
-        end, "Extract Block [t]o file")
-
+        end, "Extract Block [t]o file"),
+        {
+            "<leader>rr",
+            mode = { "n", "x" },
+            function() require('refactoring').select_refactor() end,
+            desc = "Refactor: Select [R]efactor",
+            silent = true,
+        }
+    },
+    config = function()
         require('refactoring').setup({})
     end
 }
