afef8be22296efefe0dc07948e9463ba519409e6
Allestor Oakley <allestoroakley@outlook.com>
2025-12-18 22:41:20 +0700

    feat: properly lazy load menu

--- lua/plugins/ui/menu.lua
+++ lua/plugins/ui/menu.lua
@@ -4,22 +4,26 @@ return {
         { "nvzone/volt", lazy = true },
     },
     lazy = true,
-    event = "VeryLazy",
-    config = function()
-        local default_opts = { mouse = true, border = true }
-        -- mouse users + nvimtree users!
-        vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
-            require('menu.utils').delete_old_menus()
+    keys = {
+        {
+            "<RightMouse>",
+            mode = { "n", "v" },
+            function()
+                local default_opts = { mouse = true, border = true }
+                require('menu.utils').delete_old_menus()
 
-            vim.cmd.exec '"normal! \\<RightMouse>"'
+                vim.cmd.exec '"normal! \\<RightMouse>"'
 
-            local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
-            -- for neotree
-            if vim.bo[buf].ft == "neo-tree" then
-                require("menu").open("neo-tree", default_opts)
-            else
-                require("menu").open("default", default_opts)
-            end
-        end, { desc = "Menu: Open" })
-    end
+                local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+                -- for neotree
+                if vim.bo[buf].ft == "neo-tree" then
+                    require("menu").open("neo-tree", default_opts)
+                else
+                    require("menu").open("default", default_opts)
+                end
+            end,
+            desc = "Menu: Open"
+        }
+    },
+    config = true
 }
